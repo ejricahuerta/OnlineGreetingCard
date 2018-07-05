ef7503cd20ee376b6f0fe84d6017f0829ad7bc37
ejricahuerta <ejricahuerta@10.40.12.193>
2018-07-05 11:47:52 -0400

    moved to own package

---
+++ Sendable/src/sendable/dao/interfaces/RepositoryInterface.java
@@ -0,0 +1,19 @@
+package sendable.dao.interfaces;
+
+import java.util.List;
+
+public interface RepositoryInterface<T> {
+
+	List<T> ListAll();
+
+	T Get(int id);
+
+	void Insert(T obj);
+
+	void Update(T obj);
+
+	T Find(String query);
+
+	void Remove(int id);
+
+}
