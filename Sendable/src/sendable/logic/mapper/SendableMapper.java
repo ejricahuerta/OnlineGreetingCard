059cdba972730758449d2e8f813bd5daefdda868
ejricahuerta <ejricahuerta@10.40.12.59>
2018-07-11 16:41:15 -0400

    added mapped accounts

--- Sendable/src/sendable/logic/mapper/SendableMapper.java
+++ Sendable/src/sendable/logic/mapper/SendableMapper.java
@@ -1,10 +1,17 @@
 package sendable.logic.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import sendable.dao.entities.Account;
 import sendable.dao.entities.Address;
 import sendable.dao.entities.Card;
+import sendable.dao.entities.CardLetter;
 import sendable.dao.entities.Payment;
+import sendable.logic.dtos.AccountDto;
 import sendable.logic.dtos.AddressDto;
 import sendable.logic.dtos.CardDto;
+import sendable.logic.dtos.CardLetterDto;
 import sendable.logic.dtos.PaymentDto;
 
 public class SendableMapper {
@@ -36,5 +43,17 @@ public class SendableMapper {
 	public static AddressDto MapAddress(Address address) {
 		return new AddressDto(address.getId(), address.getLine1(), address.getLine2(), address.getCity(),
 				address.getState(), address.getPostalCode());
+	}
+
+	public static AccountDto MapAccount(Account account) {
+		return new AccountDto(account.getId(), account.getUser().getId(), account.getCredit(), account.getLastTopUpDate());
+	}
+
+	public static List<CardLetterDto> MapCardLetter(List<CardLetter> cardLetters) {
+		List<CardLetterDto> allLetters = new ArrayList<CardLetterDto>();	
+		for (CardLetter c : cardLetters) {
+			allLetters.add(new CardLetterDto(c.getId(),c.getUser().getId(), c.getCard().getId(),c.getRecipient(), c.getMessage(), c.getFont(), c.getTotalCost(), c.getDateAdded()));
+		}
+		return allLetters;
 	}
 }
