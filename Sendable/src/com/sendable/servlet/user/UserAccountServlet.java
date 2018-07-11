e903e231a316c394e12da3fb6146db85199ead87
ejricahuerta <ejricahuerta@10.40.12.59>
2018-07-11 16:42:00 -0400

    added implementation for prf

--- Sendable/src/com/sendable/servlet/user/UserAccountServlet.java
+++ Sendable/src/com/sendable/servlet/user/UserAccountServlet.java
@@ -1,6 +1,8 @@
 package com.sendable.servlet.user;
 
 import java.io.IOException;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,7 +18,9 @@ import sendable.logic.services.UserService;
  */
 @WebServlet("/MyAccount")
 public class UserAccountServlet extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
+	private UserService userservice;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -35,18 +39,18 @@ public class UserAccountServlet extends HttpServlet {
 
 		HttpSession session = (HttpSession) request.getSession(false);
 		int id = (int) session.getAttribute("userId");
-		System.out.println("ID: "+ id);
+		System.out.println("ID: " + id);
 		UserService service = (UserService) session.getServletContext().getAttribute("userService");
 		UserDto user = service.FindUserById(id);
-		
-		if(user==null) {
-			
+
+		if (user == null) {
+
 			response.sendRedirect("index.jsp");
+		} else {
+			System.out.println("Current User Email: " + user.getEmail());
+			session.setAttribute("user", user);
+			response.sendRedirect("myaccount.jsp");
 		}
-		
-		System.out.println("Current User Email: " + user.getEmail());
-		session.setAttribute("user", user);
-		response.sendRedirect("myaccount.jsp");
 	}
 
 	/**
@@ -55,6 +59,49 @@ public class UserAccountServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		this.doGet(request, response);
+		HttpSession session = (HttpSession) request.getSession();
+		UserDto user = (UserDto) session.getAttribute("user");
+		int Id = (int) session.getAttribute("userId");
+		String modalType = request.getParameter("editmodal");
+		if (modalType.isEmpty()) {
+			this.doGet(request, response);
+		} else {
+
+			// validate user password
+			String password = request.getParameter("currentpassword");
+			boolean validUser = userservice.ValidateLogin(user.getEmail(), password);
+			boolean success= false;
+			if (validUser) {
+				// Edit Modal Type
+				switch (modalType) {
+				case "fullname": // edit for fullname
+					String fname = request.getParameter("firstname");
+					String lname = request.getParameter("lastname");
+					success = userservice.UpdateUserInfo(Id, fname, lname, user.getEmail());
+					break;
+				case "address": // edit for address
+					break;
+				default: // nothing then returns to page
+					this.doGet(request, response);
+					break;
+				}
+			}
+			if (success) {
+				userservice.SaveChanges();
+				System.out.println("Saved Changes");
+				UserDto updatedUser = userservice.FindUserById(Id);
+				session.setAttribute("user", updatedUser);
+			} else {
+				request.setAttribute("validationMessage", "<b>Update Failed!</b> Unable to Process Update.");
+			}
+			request.getRequestDispatcher("myaccount.jsp").forward(request, response);
+		}
+
 	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		this.userservice = (UserService) config.getServletContext().getAttribute("userService");
+	}
+
 }
