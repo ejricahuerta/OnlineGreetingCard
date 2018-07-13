ecfee98078a9115bd31dd908fea9761e6549a959
ejricahuerta <ejricahuerta@10.40.12.207>
2018-07-13 16:59:52 -0400

    added implementation for letter call back

--- Sendable/src/com/sendable/servlet/user/WriteServlet.java
+++ Sendable/src/com/sendable/servlet/user/WriteServlet.java
@@ -19,7 +19,7 @@ import sendable.logic.services.UserService;
 /**
  * Servlet implementation class WriteServlet
  */
-@WebServlet("/Write")
+@WebServlet("/Write/*")
 public class WriteServlet extends HttpServlet {
 
 	private CardService cardservice;
@@ -40,60 +40,83 @@ public class WriteServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		try {
 
-		String message = request.getParameter("message");
-		String recipient = request.getParameter("recipient");
-		String font = request.getParameter("font");
+			String message = request.getParameter("message");
+			String recipient = request.getParameter("recipient");
+			String font = request.getParameter("font");
 
-		int cardId = Integer.parseInt(request.getParameter("cardId"));
-		CardDto card = cardservice.getCard(cardId);
-		request.setAttribute("cardSelected", card);
-		
-		if (message == null || message.length() < 5) {
-			request.setAttribute("validationMessage", "<b>Invalid Message!</b> Please Enter your message again.");
+			int cardId = Integer.parseInt(request.getParameter("cardId"));
+			CardDto card = cardservice.getCard(cardId);
+			request.setAttribute("cardSelected", card);
 
-		} else if (recipient == null) {
-			request.setAttribute("validationMessage", "<b>Invalid Recipient!</b> Please Enter your message again.");
-			request.getRequestDispatcher("write.jsp").forward(request, response);
-		} else {
-			String button = request.getParameter("button");
-			int userId = (int) request.getSession().getAttribute("userId");
-			CardLetterDto newLetter = new CardLetterDto(0, userId, cardId, recipient, message, font,
-					card.getPrice(), DateTime.GetCurrentDate());
-			int letterId = this.userservice.addUserLetter(userId, newLetter);
+			if (message == null || message.length() < 5) {
+				request.setAttribute("validationMessage", "<b>Invalid Message!</b> Please Enter your message again.");
 
-			if (letterId != -1) {
-				this.userservice.saveChanges();
-				UserDto updatedUser = this.userservice.findUserById(userId);
-				request.getSession().setAttribute("user", updatedUser);
-				newLetter = this.userservice.getUserLetter(userId, letterId);
-				request.setAttribute("letter", newLetter);
-				if(button.equals("Pay Now")) {
-					request.getRequestDispatcher("payment.jsp").forward(request, response);
-				}
-				else {
-					request.getRequestDispatcher("myaccount.jsp").forward(request, response);
-				}
-				
-			} else {
-				request.setAttribute("validationMessage", "<b>Unable to Proccess!</b>Please Re-Enter Message.");
+			} else if (recipient == null) {
+				request.setAttribute("validationMessage", "<b>Invalid Recipient!</b> Please Enter your message again.");
 				request.getRequestDispatcher("write.jsp").forward(request, response);
-			}
-		}
+			} else {
+				String button = request.getParameter("button");
+				int userId = (int) request.getSession().getAttribute("userId");
+				CardLetterDto newLetter = new CardLetterDto(0, userId, cardId, recipient, message, font,
+						card.getPrice(), DateTime.GetCurrentDate());
+				int letterId = this.userservice.addUserLetter(userId, newLetter);
 
+				if (letterId != -1) {
+					this.userservice.saveChanges();
+					UserDto updatedUser = this.userservice.findUserById(userId);
+					request.getSession().setAttribute("user", updatedUser);
+					newLetter = this.userservice.getUserLetter(userId, letterId);
+					request.setAttribute("letter", newLetter);
+					if (button.equals("Pay Now")) {
+						request.getRequestDispatcher("payment.jsp").forward(request, response);
+					} else {
+						response.sendRedirect("myaccount.jsp");
+					}
+
+				} else {
+					request.setAttribute("validationMessage", "<b>Unable to Proccess!</b>Please Re-Enter Message.");
+					request.getRequestDispatcher("write.jsp").forward(request, response);
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("ID: " + request.getParameter("cardId"));
-		if (request.getParameter("cardId") == null) {
-			response.sendRedirect(request.getContextPath() + "/card.jsp");
-		} else {
-			int id = Integer.parseInt(request.getParameter("cardId"));
-			CardDto card = cardservice.getCard(id);
-			request.setAttribute("cardSelected", card);
-			request.getRequestDispatcher("write.jsp").forward(request, response);
+		System.out.println("CARD ID: " + request.getParameter("cardId"));
+		System.out.println("LETTER ID" + request.getParameter("letterId"));
+		try {
+			if (request.getParameter("cardId") == null) {
+				if(request.getParameter("letterId") != null) {
+					int letterId = Integer.parseInt(request.getParameter("letterId"));
+					int userId = (int)request.getSession().getAttribute("userId");
+					CardLetterDto letter =  this.userservice.getUserLetter(userId, letterId);
+					System.out.println(letter.getMessage());
+					if(letter != null) {
+						request.setAttribute("message", letter.getMessage());
+						request.setAttribute("recipient", letter.getRecipient());
+						request.getRequestDispatcher("Write?cardId="+letter.getCardId()).forward(request, response);
+						
+					}
+					
+				}
+				else {
+				response.sendRedirect(request.getContextPath() + "/cards.jsp");
+				}
+			} else {
+				int id = Integer.parseInt(request.getParameter("cardId"));
+				CardDto card = cardservice.getCard(id);
+				request.setAttribute("cardSelected", card);
+				request.getRequestDispatcher("write.jsp").forward(request, response);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+
 	}
 
 	@Override
