c798b1b900c9a5d97bd3535789382a5604e0b5cc
ejricahuerta <exricahuerta@gmail.com>
2018-07-14 16:04:58 -0400

    clean up naming and tags

--- Sendable/src/com/sendable/servlet/authentication/LoginServlet.java
+++ Sendable/src/com/sendable/servlet/authentication/LoginServlet.java
@@ -3,14 +3,11 @@ package com.sendable.servlet.authentication;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import data.mock.MockService;
-import sendable.dao.entities.User;
 import sendable.logic.dtos.UserDto;
 import sendable.logic.services.UserService;
 
