ad30488527e63225bd9f821c496b979fb455c7a7
exricahuerta@gmail.com <exricahuerta@gmail.com>
2018-07-17 02:06:35 -0500

    added session check

--- Sendable/src/com/sendable/servlet/user/PaymentServlet.java
+++ Sendable/src/com/sendable/servlet/user/PaymentServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import sendable.logic.services.PaymentService;
 
@@ -37,7 +38,12 @@ public class PaymentServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = (HttpSession) request.getSession(false);
+		if(session == null) {
+			response.sendRedirect("login.jsp");
+		}else {			
 			request.getRequestDispatcher("pay.jsp").forward(request, response);
+		}
 	}
 
 	/**
