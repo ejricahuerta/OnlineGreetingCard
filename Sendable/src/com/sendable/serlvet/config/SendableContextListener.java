c8ae0cc960ee02202ace90f8734094249b7519fc
exricahuerta@gmail.com <exricahuerta@gmail.com>
2018-08-01 03:47:45 -0500

    Final Version

--- Sendable/src/com/sendable/serlvet/config/SendableContextListener.java
+++ Sendable/src/com/sendable/serlvet/config/SendableContextListener.java
@@ -2,14 +2,14 @@ package com.sendable.serlvet.config;
 
 import java.util.List;
 
+import javax.management.RuntimeErrorException;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 import data.mock.SeedDatabase;
 import sendable.dao.database.DatabaseManager;
-import sendable.dao.entities.Card;
-import sendable.dao.entities.Category;
+
 import sendable.dao.interfaces.DatabaseManagerInterface;
 import sendable.dao.interfaces.UnitOfWorkInterface;
 import sendable.dao.repository.UnitOfWork;
@@ -45,21 +45,25 @@ public class SendableContextListener implements ServletContextListener {
 	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
 	 */
 	public void contextInitialized(ServletContextEvent arg0) {
-		
-		DatabaseManagerInterface database = new DatabaseManager();
-		UnitOfWorkInterface uow = new UnitOfWork(database);
-		if(uow.GetCategoryRepo().ListAll().isEmpty()) {
-			SeedDatabase seed = new SeedDatabase(uow);
-			seed.SeedCard();
-			
+		try {
+			DatabaseManagerInterface database = new DatabaseManager();
+			UnitOfWorkInterface uow = new UnitOfWork(database);
+			if(uow.GetCategoryRepo().ListAll().isEmpty()) {
+				SeedDatabase seed = new SeedDatabase(uow);
+				seed.SeedCard();
+				
+			}
+			CardInterface cardservice = new CardService(uow);
+			List<CardDto> allcards = cardservice.ListCards();
+			List<CategoryDto> allcat = cardservice.ListCategories();
+			arg0.getServletContext().setAttribute("allcards", allcards);
+			arg0.getServletContext().setAttribute("allcategories", allcat);
+			arg0.getServletContext().setAttribute("cardService", cardservice);
+			arg0.getServletContext().setAttribute("userService", new UserService(uow));
+			arg0.getServletContext().setAttribute("paymentService", new PaymentService(uow));
+		} catch (Exception e) {
+			System.out.println("Unable to Load Database!");
 		}
-		CardInterface cardservice = new CardService(uow);
-		List<CardDto> allcards = cardservice.ListCards();
-		List<CategoryDto> allcat = cardservice.ListCategories();
-		arg0.getServletContext().setAttribute("allcards", allcards);
-		arg0.getServletContext().setAttribute("allcategories", allcat);
-		arg0.getServletContext().setAttribute("cardService", cardservice);
-		arg0.getServletContext().setAttribute("userService", new UserService(uow));
-		arg0.getServletContext().setAttribute("paymentService", new PaymentService(uow));
+		
 	}
 }
